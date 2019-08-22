4a052ab2fd0838654f3184c87e3685b4446fca0f
emmanuelkamala <49253728+emmanuelkamala@users.noreply.github.com>
2019-08-22 19:32:08 +0300

    added routes to routes file

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,14 @@ Rails.application.routes.draw do
   resources :microposts
   resources :toys
 
-  get 'toys'
-  get 'microposts'
+  get 'toys_controller#index'
+  get 'toys_controller#create'
+  get 'toys_controller#edit'
+  get 'toys_controller#destroy'
+  get 'toys_controller#new'
+  get 'toys_controller#show'
+  get 'toys_controller#update'
+  get 'toys_controller#destroy'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
