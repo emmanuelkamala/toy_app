7ef0e42b479a125ef01c28717dd5171c24f88a2b
emmanuelkamala <49253728+emmanuelkamala@users.noreply.github.com>
2019-08-22 19:21:46 +0300

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   resources :microposts
   resources :toys
+
+  get 'toys'
+  get 'microposts'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
